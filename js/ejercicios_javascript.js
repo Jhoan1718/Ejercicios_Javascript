ac348f9e056a02fe7908ea5f5cecf16597214fe8
johan sebastian <sr.jhoan16@gmail.com>
2022-02-07 16:10:25 -0500

    add ejercicios

--- js/ejercicios_javascript.js
+++ js/ejercicios_javascript.js
@@ -1,9 +1,22 @@
+"Use strict"
 //ej 1
 console.log("hola mundo");
 // ej 
 
 
-var nombre = "(sr.)jhoan Sebastian"
+var nombre = "(sr.)jhoan Sebastian";
 if(nombre){
-    console.log("bienvenido"+nombre)
+    console.log("bienvenido"+" "+nombre)
 };
+
+let costoProducto = 20000;
+let domicilio = 5000;
+let impuesto = 0.10;
+
+
+console.log("el valor inicial es"+" "+costoProducto)
+let subtotal = costoProducto + (costoProducto*impuesto);
+console.log ("el valor subtotal"+" "+subtotal);
+let valortotal = (subtotal+domicilio);
+console.log ("el valor total"+" "+valortotal);
+
