fc99756f859de19f0f61e70939b90ba4d668862e
unknown <SENA@PITAPRTNNFSD271.FormacionSena.RED>
2022-02-09 16:08:21 -0500

    añadiendo arreglo

---
+++ js/arreglo.js
@@ -0,0 +1,11 @@
+"use strict"
+
+let usuario = new Array ();
+usuario[0]= "yo";
+usuario[1]= "tu";
+usuario[2]= "el";
+usuario[3]= "ella";
+
+for (var j= 0; j <= usuario.length -1; j++){
+    console.log (usuario[j])
+}
