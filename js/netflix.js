bbe9dd68e41a954ca56ba6b91116718e728f4b6c
Johan Sebastian <sr.jhoan16@gmail.com>
2022-02-21 16:33:38 -0500

    add java1

--- js/netflix.js
+++ js/netflix.js
@@ -1,2 +1,22 @@
 "Use strict"
+function verInfo(){
+    var nombreUsuario = document.getElementById("nombre").value;
+    alert("bienvenido,sr. "+ nombreUsuario);
+    var texto = documnet.getElementById("parr").innerHTML;
+    alert(texto);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
