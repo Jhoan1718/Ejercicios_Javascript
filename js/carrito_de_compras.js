676fb3b6278a7e2f674f22e7545a618f4a0243fb
adsi <adsi@PITAPRTNNFSD215.FormacionSena.RED>
2022-02-28 17:27:25 -0500

    añadiendo carrito

---
+++ js/carrito_de_compras.js
@@ -0,0 +1,30 @@
+let inicio = 0;
+let numUnidades = "";
+    
+function aumentar(){
+     if( inicio <= 9) {
+         numUnidades = document.getElementById('numUnidades').value = ++inicio;
+         document.getElementById("total").innerHTML = cantidad;
+            
+        }
+    }
+function disminuir(){
+     if( inicio >= 1) {
+          numUnidades = document.getElementById('numUnidades').value = --inicio;
+        }
+    }
+
+function calcularTotal() {
+    let valorUnidad = document.getElementById("valorUnitario").innerHTML;
+    let cantidadElementos = document.getElementById("total").innerHTML;
+    let valorCompra = valorUnidad * cantidadElementos;
+    document.getElementById("multiplicar").innerHTML = valorCompra;
+    
+}
+
+
+
+
+
+
+
