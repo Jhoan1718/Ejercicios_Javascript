220df0bd3128bd9d71d489014c6d9ee4f1f2213c
Johan Sebastian <sr.jhoan16@gmail.com>
2022-03-07 10:23:51 -0500

    añadiendo mucho ams archivos del carrto

--- js/carrito_de_compras.js
+++ js/carrito_de_compras.js
@@ -1,12 +1,14 @@
-let inicio = 0;
+/* let inicio = 0;
 let numUnidades = "";
     
 function aumentar(){
      if( inicio <= 9) {
          numUnidades = document.getElementById('numUnidades').value = ++inicio;
+
+          */
          /* document.getElementById("total").innerHTML = cantidad; */
             
-        }
+     /*    }
         calcularSubtotal();
     }
 function disminuir(){
@@ -20,12 +22,40 @@ function disminuir(){
 
 function calcularSubtotal() {   
     let valorUnidad = document.getElementById("numUnidades").innerHTML;
-    alert(total)
+   
     let cantidadElementos = document.getElementById("total").innerHTML;
     let valorCompra = valorUnidad * cantidadElementos;
     document.getElementById("multiplicar").innerHTML = valorCompra;
     
 }
+ */
+
+
+let inicio = 0;
+let cantidad = "";
+
+
+function aumentar(){
+    if( inicio <= 9) {
+        cantidad = document.getElementById('cantidad').value = ++inicio;
+        document.getElementById("total").innerHTML = cantidad;
+    }
+    calcularTotal();
+}
+ 
+function disminuir() {
+    if(inicio >= 1) {
+        cantidad = document.getElementById('cantidad').value = --inicio;
+    }
+}
+function calcularTotal() {
+    let valorUnidad = document.getElementById("valorUnitario").innerHTML;
+    let cantidadElementos = document.getElementById("total").innerHTML;
+    let valorCompra = valorUnidad * cantidadElementos;
+    document.getElementById("multiplicar").innerHTML = valorCompra;
+}
+
+
 
 
 
